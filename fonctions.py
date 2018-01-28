ed6a0d5c0a82145cf555808219261e7b3e2b2c62
Mathieu Logario <mathieu.logario@ensta-bretagne.org>
2018-01-28 22:08:36 +0100

    ajout de fonctions.py

---
+++ fonctions.py
@@ -0,0 +1,45 @@
+def add(r1,o):
+	return(r1+o)
+
+def sub(r1,o):
+	return(r1-o)
+
+def mult(r1,o):
+	return(r1*o)
+
+def div(r1,o):
+	return(r1/o)
+
+def et(r1,o):
+	return(r1&o)
+
+def ou(r1,o):
+	return(r1|o)
+
+def xor(r1,o):
+	return(r1~o)
+
+def shl(r1,o):
+	return(r1<<o)
+
+def shr(r1,o):
+	return(r1>>o)
+
+def slt(r1,o):
+	if r1<o:
+		return(1)
+	else:
+		return(0)
+
+def sle(r1,o):
+	if r1<=0:
+		return(1)
+	else:
+		return(0)
+
+def seq(r1,o)
+	if r1==0:
+		return(1)
+	else:
+		return(0)
+	
