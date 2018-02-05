988237bdf7e219b451cf80cc2ab26ecdde092db5
charlie.goutorbe@gmail.com <charlie.goutorbe@gmail.com>
2018-02-05 10:03:26 +0100

    traducteur

--- traducteur.py
+++ traducteur.py
@@ -109,14 +109,28 @@ def convertParam(params,instru):
             conversion = []
     return (conversion)
 
-def split_ligne():
+def split_ligne(tableLabel,compteur):
     fichier='asm.txt'
+    
     with open(fichier, 'r') as human:
             ligne = human.readline()
             print(ligne)
+            #on cherche si il y a un label
+            labs = ligne.split(': ')
+            if  labs[0][0]=='L':
+                #il y a un label
+                tableLabel.append((labs[0],compteur))
+                print("label enregistré",labs)
+                print("==========================")
+                ligne =  labs[1]
+                print(tableLabel)
+                print("ligne sans label\n")
+                print(ligne)
+
             instru, registers = ligne.split(' ')[0], ligne.split(' ')[1]
             nb_param = getNbParameters(convertInstruction(instru))
             params = splitParam(registers,nb_param)
+
     return(instru,params)
 
 def listToBin(l,params):
@@ -149,46 +163,28 @@ def listToBin(l,params):
             hexa+='\n'
             with open('bin.txt', 'w') as output:
                 output.write(hexa)
+
     elif(params == 1):
         binaire+=l[1]
         hexa=hex(binaire)
         hexa+='\n'
         with open('bin.txt', 'w') as output: 
             output.write(hexa)
+
     else:
         hexa=hex(binaire)
         hexa+='\n'
         with open('bin.txt', 'w') as output:
             output.write(hexa)
-
-def label():
-    """on parcout la première colonne on stocke les valeurs des labels et on remplace par les adresses correspondantes"""
-    fichier = 'asm.txt'
-    tableLabel = []
-    compteLigne =0
-    with open(fichier, 'r') as human:
-        for ligne in human.readlines():
-            compteLigne+=1 #donne la valeur de la ligne
-            print(ligne)
-            if ligne[0] == 'L':
-                #on a un Label
-                tableLabel.append([ligne.split(':')[0],compteLigne])
-                #on ajoute le nom du label et le numéro de ligne correspondan 
-        print(tableLabel)    
-
-def remplaceLabel(tableLabel):
-    fichier = 'asm.txt'
-    fichiero = 'asm.o'
-    fp = open(fichiero,'w')
-    with open(fichier,'r') as human:
-        for ligne in human.readlines():
-            for L in tableLabel[:0]:
-                print(L)
+                    
+            #print(tableLabel[i][0])
         
 def main():
-    tableLabel = label()
-    remplaceLabel(tableLabel)
-    instru, params = split_ligne()
+    #tableLabel = label()
+    #remplaceLabel(tableLabel)
+
+    tableLabel = []    
+    instru, params = split_ligne(tableLabel,1)
     cvted_inst = [convertInstruction(instru)]
     cvted_param = convertParam(params,instru)
     cvted_all = cvted_inst+cvted_param
